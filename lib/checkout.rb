6c9f46201c1b0c64f70d9008e1dae441fafddc6c
charlescampbell <charlie.campbell14@googlemail.com>
2021-09-02 15:15:09 +0100

    refactor(total): Move logic for adding no discount out

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 class Checkout
   attr_reader :prices
   private :prices
@@ -20,7 +22,7 @@ class Checkout
         if (count % 2 == 0)
           total += prices.fetch(item) * (count / 2)
         else
-          total += prices.fetch(item) * count
+          total += apply_no_discount(item, count)
         end
       elsif item == :banana || item == :pineapple
         if item == :pineapple
@@ -30,7 +32,7 @@ class Checkout
           total += (prices.fetch(item) / 2) * count
         end
       else
-        total += prices.fetch(item) * count
+        total += apply_no_discount(item, count)
       end
     end
 
@@ -46,5 +48,9 @@ class Checkout
   # TODO: Investigate looking at each_with_object
   def itemized_basket
     basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }
+  end
+
+  def apply_no_discount(item, count)
+    prices.fetch(item) * count
   end
 end
