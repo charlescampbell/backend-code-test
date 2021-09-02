1884b1fea723e54d510af26660d56df7c653469e
charlescampbell <charlie.campbell14@googlemail.com>
2021-09-02 15:35:25 +0100

    refactor(total): Use case over if

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -18,25 +18,26 @@ class Checkout
     total = 0
 
     itemized_basket.each do |item, count|
-      if item == :apple || item == :pear
-        if (count % 2 == 0)
-          total += prices.fetch(item) * (count / 2)
-        else
-          total += apply_no_discount(item, count)
-        end
-      elsif item == :banana || item == :pineapple
+      case item
+      when :apple, :pear
+        total += if (count % 2).zero?
+                   prices.fetch(item) * (count / 2)
+                 else
+                   apply_no_discount(item, count)
+                 end
+      when :banana, :pineapple
         if item == :pineapple
           total += (prices.fetch(item) / 2)
-          total += (prices.fetch(item)) * (count - 1)
+          total += prices.fetch(item) * (count - 1)
         else
           total += (prices.fetch(item) / 2) * count
         end
-      elsif item == :mango
-        if (count % 4 == 0)
-          total += prices.fetch(item) * (count * 0.75)
-        else
-          total += apply_no_discount(item, count)
-        end
+      when :mango
+        total += if (count % 4).zero?
+                   prices.fetch(item) * (count * 0.75)
+                 else
+                   apply_no_discount(item, count)
+                 end
       else
         total += apply_no_discount(item, count)
       end
